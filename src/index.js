65eb531b8b6070e4700b10f8963c2610c0b16f3e
s_minnikin@hotmail.com <mini@minis-iMac.local>
2018-01-06 16:05:54 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react';  // Imports the core React library which works with react components
+import ReactDom from 'react-dom'; // used to add components to the DOM (the page)
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY  = 'AIzaSyBBv8MK66S0N2z82eydAzZwPZj1ZavOTyY';  // API_KEY for youtube
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// create a new component
+// component should produce some HTML
+// this is a factory a class and not an instance of the component
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () =>  {       // ES6 const is used for a App, so the value returned to it will not change
+    return <div>Hi!</div>;      // this is JSX, which looks like HTML, but is actually javascript
+}
+
+
+// Take this components HTMl and put it on the page (in the DOM)
+//the first argument is the component and the second is the target container
+ReactDom.render(<App />, document.querySelector('.container'));  //We are passing an instance now.  The tag creates an instance
