f2a0f94a1c914fee9f5ba9c79c0aade32304edcd
s_minnikin@hotmail.com <mini@minis-iMac.local>
2018-01-06 16:16:03 +0000

    searchBar component created

---
+++ src/components/searchBar.js
@@ -0,0 +1,10 @@
+import React from 'react'; // needs to be imported into all components
+
+
+const SearchBar = () => {
+    return <input />;        // returns an input element
+};
+
+export default SearchBar;  // this will export the component
+
+
