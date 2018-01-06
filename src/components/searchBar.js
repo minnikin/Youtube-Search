591174ce0ee9e24e910538412a51a895e4cdea60
s_minnikin@hotmail.com <mini@minis-iMac.local>
2018-01-06 19:18:21 +0000

    Converted from functional component to class based component

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,9 +1,17 @@
-import React from 'react'; // needs to be imported into all components
+import React, {Component} from 'react'; // needs to be imported into all components
 
 
+class SearchBar extends Component  {
+    render() {
+        return <input />;
+    }
+}
+
+// functional component
+/*
 const SearchBar = () => {
     return <input />;        // returns an input element
-};
+}; */
 
 export default SearchBar;  // this will export the component
 
