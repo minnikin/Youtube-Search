c30c424fe79a8ce927d0a9b46de4a05f8eaff3c1
s_minnikin@hotmail.com <mini@minis-iMac.local>
2018-01-07 13:20:49 +0000

    Added constructor and onChange event

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -2,9 +2,25 @@ import React, {Component} from 'react'; // needs to be imported into all compone
 
 
 class SearchBar extends Component  {
-    render() {
-        return <input />;
+
+    // We use the constructor to setup the state
+    constructor(props) {
+        super(props); // this calls the constructor on the parent class Component
+
+        // state is an object available to all classes, only inside the constructor do we change state this way
+        this.state = { term: '' }; // we are recording the state of the search term on initialisation
     }
+    
+    // the render function uses a fat arrow to take an event and use the value to set the state
+    render() {
+        return (
+        <div>
+            <input onChange = {event => this.setState({ term: event.target.value})} />
+            Value of the input: {this.state.term}
+        </div>
+        );
+    }
+
 }
 
 // functional component
